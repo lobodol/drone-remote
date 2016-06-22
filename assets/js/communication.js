5b98ef20f28fb047d44bc98f43dedf8f59fc0e5e
lobodol <grobodol@gmail.com>
2016-06-22 14:25:05 +0200

    Add method to send instructions to Arduino

--- assets/js/communication.js
+++ assets/js/communication.js
@@ -1,4 +1,4 @@
-var app = {
+var Communication = {
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
@@ -72,7 +72,34 @@ var app = {
         off.onclick = function() {
             if (open) serial.write('0');
         }
+    },
+
+    /**
+     * Build message as an array of bytes and sent it to Arduino.
+     *
+     * @param number yaw      : yaw command [0, 360]
+     * @param number pitch    : pitch command [0, 900]
+     * @param number roll     : roll command [0, 900]
+     * @param number throttle : throttle command [0, 100]
+     */
+    sendInstructions: function(yaw, pitch, roll, throttle) {
+        var message = new Uint8Array(10);
+        message[0] = 0x02;
+        message[1] = yaw >> 8;   // Yaw MSB
+        message[2] = yaw;        // Yaw LSB
+        message[3] = pitch >> 8; // Pitch MSB
+        message[4] = pitch;      // Pitch LSB
+        message[5] = roll >> 8;  // Roll MSB
+        message[6] = roll;       // Roll LSB
+        message[7] = throttle;   // Throttle
+        var checksum = message[0] ^ message[1] ^ message[2] ^ message[3] ^ message[4] ^ message[5] ^ message[6] ^ message[7];
+        message[8] = checksum;
+        message[9] = 0x03;
+
+        for (var i = 0; i < message.length; i++) {
+            serial.write(message[i]);
+        }
     }
 };
 
-app.initialize();
+Communication.initialize();
